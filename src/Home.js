dc43a4940c38bcea2384974fb85e28cfc6c9f577
Katelinn Jones <katiejonesyo@gmail.com>
2020-11-11 18:09:35 -0800

    trying to debug netlify

--- src/Home.js
+++ src/Home.js
@@ -9,7 +9,7 @@ export default class Home extends Component {
                 </h1>
                 <body>
                     
-                   <center> <iframe src="https://giphy.com/embed/xUPJPhqYSbfL6OXrUY" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></center>
+                   <center> <iframe src="https://giphy.com/embed/xUPJPhqYSbfL6OXrUY" width="480" height="360" frameBorder="0" class="giphy-embed" title="uniquetitle" alt="unique"allowFullScreen></iframe></center>
                     
                 </body>
             </main>
